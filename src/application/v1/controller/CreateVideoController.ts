0b3246e42a172b0024957aedbebfcd6398e4735b
Sabrina Chaves <sabrinach08@gmail.com>
2023-12-05 10:35:46 -0300

    feat: add create video controller

---
+++ src/application/v1/controller/CreateVideoController.ts
@@ -0,0 +1,32 @@
+import { buildErrorInfo } from '@infrastructure/parser/ErrorInfo';
+import ICreateVideoDTO from '@service/CreateVideo/interfaces/ICreateVideoDTO';
+import ICreateVideoService from '@service/CreateVideo/interfaces/ICreateVideoService';
+import { RequestHandler } from 'express';
+import { CREATED } from 'http-status';
+
+export default class CreateVideoController {
+  constructor(private createVideoService: ICreateVideoService) {}
+
+  public execute: RequestHandler = async (request, response) => {
+    try {
+      const { title, description, url } = request.body;
+
+      const createVideoDTO: ICreateVideoDTO = {
+        title,
+        description,
+        url,
+      };
+
+      const video = await this.createVideoService.handle(createVideoDTO);
+
+      console.log(`Video created - id ${video._id}`);
+
+      return response.status(CREATED).json(video);
+    } catch (err: any) {
+      const errorInfo = buildErrorInfo(err);
+      const responseData = { error: errorInfo.errorMessage };
+
+      return response.status(errorInfo.code).json(responseData);
+    }
+  };
+}
