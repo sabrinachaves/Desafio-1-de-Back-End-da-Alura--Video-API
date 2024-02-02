eaca3112ada458d487a72c33f267c58d20087a62
Sabrina Chaves <sabrinach08@gmail.com>
2024-02-02 18:57:59 -0300

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import AppFactory from '@infrastructure/factories/AppFactory';
 import DatabaseFactory from '@infrastructure/factories/persistence/DatabaseFactory';
+import routes from './application/routes';
 
 async () => {
   const database = await DatabaseFactory.make();
   await database.connect();
+  const app = await AppFactory.make(routes);
+  await app.listen();
 };
